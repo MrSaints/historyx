9774f718a3694724699d89c15e7022a70da46032
Ian Lai <MrSaints@users.noreply.github.com>
2015-09-15 09:02:24 +0100

    Removed use of statics, and added full date for stale links

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -2,25 +2,21 @@ let React = require("react");
 let moment = require("moment");
 
 let HistoryItem = React.createClass({
-  statics: {
-    truncate: function (s) {
-      let newString = s.substr(0, 100);
-      if (s.length > 100) {
-        newString += "..."
-      }
-      return newString
-    },
-    getTime: function (t) {
-      return moment(t).format("hh:mm:ss A");
+  _truncate: function (s) {
+    let newString = s.substr(0, 100);
+    if (s.length > 100) {
+      newString += "..."
     }
+    return newString
   },
   render: function () {
-    let formattedTime = this.constructor.getTime(this.props.visited);
+    let formattedTime = moment(this.props.visited).format("hh:mm:ss A");
+    if (this.props.stale) {
+      formattedTime = (<em>{moment(this.props.visited).format("hh:mm A DD-MM-YYYY")}</em>);
+    }
     return (
       <tr>
-        <td className="mdl-data-table__cell--non-numeric">
-          {this.props.stale ? <em>{formattedTime}</em> : formattedTime}
-        </td>
+        <td className="mdl-data-table__cell--non-numeric">{formattedTime}</td>
         <td className="mdl-data-table__cell--non-numeric"><img src={"chrome://favicon/" + this.props.url} /></td>
         <td className="mdl-data-table__cell--non-numeric">
           <a
@@ -28,7 +24,7 @@ let HistoryItem = React.createClass({
             target="_blank"
             className="mdl-badge"
             data-badge={this.props.count}>
-            {this.constructor.truncate(this.props.title || this.props.url)}
+            {this._truncate(this.props.title || this.props.url)}
           </a>
         </td>
       </tr>
