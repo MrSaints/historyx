f4d30da1a315915727f66a0e7c25816947b02396
Ian Lai <ian@fyianlai.com>
2018-02-13 00:19:01 +0000

    Set page title to `History` for familiarity

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -11,7 +11,7 @@ module.exports = {
                     },
                 },
                 html: {
-                    title: "HistoryX (3)",
+                    title: "History",
                 },
             },
         ],
